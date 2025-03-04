d93109ceede48228dfb52d7fa981ba964725895a
ifer <361301399@qq.com>
2025-03-04 17:19:09 +0800

    feat: 系统设置接口

---
+++ seeders/20250304090648-setting.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert(
+      "Settings",
+      [
+        {
+          name: "千里封冰",
+          icp: "豫 ICP 备 2024065800 号",
+          copyright: "© 2024 Qianli Bingfeng Inc. All Rights Reserved.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Settings", null, {});
+  }
+};
